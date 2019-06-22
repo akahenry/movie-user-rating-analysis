648f089d96d7420e928df0bb78580bfa00bbc577
Henry <38134284+akahenry@users.noreply.github.com>
2019-06-22 16:59:24 -0300

    UPD - hash, trie, main e readFiles.
    Todos esses arquivos foram modificados e comentados.

--- trie.py
+++ trie.py
@@ -1,42 +1,91 @@
 class trieNode():
     def __init__(self):
        self.children = []
-       self.char = None
-       self.value = None  
-       
-    def insert(self, key, value):
+       self.key = None
+       self.data = None
+
+    # insert: string Any -> trieNode
+    # Objetivo: dada uma árvore trie, uma string e dados satélites, essa função
+    #   insere essa string na árvore trie segundo o prefixo da string dada.
+    def insert(self, key, data):
         node = self
         for char in key:
             found = False
             for child in node.children:
-                if child.char == char:
+                if child.key == char:
                     found = True
                     node = child
-            
+
             if not found:
                 auxnode = trieNode()
-                auxnode.char = char
+                auxnode.key = char
                 node.children.append(auxnode)
                 node = node.children[-1]
-        node.value = value
-                 
+        node.data = data
+
+    # find: string -> list
+    # Objetivo: dada um prefixo e uma trie, a função verifica se o prefixo
+    #   está na trie, retornando uma lista de tuplas dadas por:
+    #   (string, data), onde:
+    #   - string: palavra inteira que contém o prefixo dado como entrada.
+    #   - data: dados satélites dessa palavra que estão contidos na trie.
     def find(self, key):
         node = self
         for char in key:
-            found = False
+            flag = False
             for child in node.children:
-                if child.char == char:
-                    found = True
+                if child.key == char:
                     node = child
-            
-            if not found:
-                return None
-        return node.value
-        
+                    flag = True
+                    break
+            if not flag:
+                return []
+
+        def listByPrefix(node, string):
+            list = []
+            if node.data != None:
+                list.append((string, node.data))
+            for child in node.children:
+                str = string
+                str += child.key
+                list += listByPrefix(child, str)
+            return list
+
+        return listByPrefix(node, key)
+
+
+    # def listByPrefix(self, key):
+    #     node = self
+    #     string = key
+    #     for char in key:
+    #         for child in node.children:
+    #             if child.key == char:
+    #                 node = child
+    #                 break
+    #
+    #     if node.children == None:
+    #         if node.key == key[-1] and node.data != None:
+    #             return (string, [])
+    #     else:
+    #         for child in node.children:
+
+
+
     def print(self):
         for child in self.children:
-            print(child.char)
+            print(child.key)
             child.print()
-        
-    
 
+# trie = trieNode()
+# op = 1
+# while op != 0:
+#     op = int(input("0-Sair\n1-Inserir\n2-Printar\n3-Listar por prefixo\n"))
+#     if op == 1:
+#         word = input("Word: ")
+#         data = input("data: ")
+#         trie.insert(word, data)
+#     elif op == 2:
+#         trie.print()
+#     elif op == 3:
+#         list = trie.find(input("Prefix: "))
+#         print(list)
